4ef44fa9450785c471b4639738110cbdb8078b23
Jonathan Ato Markin <jona10711@gmail.com>
2023-04-01 00:29:42 +0200

    Register route added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,29 +1,30 @@
 import express from "express";
 import {
-  authUser,
-  registerUser,
-  getUserProfile,
-  updateUserProfile,
-  getUsers,
-  deleteUser,
-  getUserById,
-  updateUser,
+    authUser,
+    registerUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    deleteUser,
+    getUserById,
+    updateUser,
 } from "../controllers/userController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(registerUser).get(protect, admin, getUsers);
+router.get("/", protect, admin, getUsers);
+router.post("/register", registerUser);
 router.post("/login", authUser);
 router
-  .route("/profile")
-  .get(protect, getUserProfile)
-  .put(protect, updateUserProfile);
+    .route("/profile")
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile);
 
 router
-  .route("/:id")
-  .delete(protect, admin, deleteUser)
-  .get(protect, admin, getUserById)
-  .put(protect, admin, updateUser);
+    .route("/:id")
+    .delete(protect, admin, deleteUser)
+    .get(protect, admin, getUserById)
+    .put(protect, admin, updateUser);
 
 export default router;
