a01110d31b16cb5f1b315def74dbb01df47754a0
Jonathan Ato Markin <jona10711@gmail.com>
2023-04-18 20:46:02 +0200

    Response codes modified for booking controller

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -5,72 +5,82 @@ import Booking from "../models/bookingModel.js";
 //@route POST /api/booking
 //@access Private
 const addBookingItems = asyncHandler(async (req, res) => {
-    const { restaurantId, tableId, date, tableName, restaurantName, customerName, dateFrom, dateTo, image } = req.body;
+  const {
+    restaurantId,
+    tableId,
+    date,
+    tableName,
+    restaurantName,
+    customerName,
+    dateFrom,
+    dateTo,
+    image,
+  } = req.body;
 
-    const booking = new Booking({
-        user: req.user._id,
-        restaurant: restaurantId,
-        table: tableId,
-        tableName: tableName,
-        restaurantName: restaurantName,
-        customerName: customerName,
-        dateFrom: dateFrom,
-        dateTo: dateTo,
-        image: image
+  const booking = new Booking({
+    user: req.user._id,
+    restaurant: restaurantId,
+    table: tableId,
+    tableName: tableName,
+    restaurantName: restaurantName,
+    customerName: customerName,
+    dateFrom: dateFrom,
+    dateTo: dateTo,
+    image: image,
+  });
+
+  const createdBooking = await booking.save();
+
+  if (createdBooking) {
+    res.status(200).json({
+      status: "00",
+      message: "Booking created successfully",
+      booking: createdBooking,
     });
-
-    const createdBooking = await booking.save();
-
-    if (createdBooking) {
-        res.status(200).json({
-            status: "SUCCESS",
-            message: "Booking created successfully",
-            booking: createdBooking,
-        });
-    } else {
-        res.status(401).json({
-            status: "FAILED",
-            message: "Booking could not be created",
-            booking: "",
-        });
-    }
+  } else {
+    res.status(401).json({
+      status: "99",
+      message: "Booking could not be created",
+      booking: "",
+    });
+  }
 });
 
 //@desc GET booking by id
 //@route GET /api/booking/:id
 //@access Private
 const getBookingById = asyncHandler(async (req, res) => {
-    const booking = await Booking.findById(req.params.id);
+  const booking = await Booking.findById(req.params.id);
 
-    if (booking) {
-        res.json(booking);
-    } else {
-        res.status(404);
-        throw new Error("Booking not found");
-    }
+  if (booking) {
+    res.json(booking);
+  } else {
+    res.status(404);
+    throw new Error("Booking not found");
+  }
 });
 
 //@desc Get logged in user bookings
 //@route GET /api/bookings/mybookings
 //@access Private
 const getMyBookings = asyncHandler(async (req, res) => {
-    const bookings = await Booking.find({ user: req.user._id });
+  const bookings = await Booking.find({ user: req.user._id });
 
-    if (bookings) {
-        res.status(200).json({
-            status: "SUCCESS",
-            message: "Bookings retrieved successfully",
-            bookings: bookings,
-        })
-    }else{
-        res.status(401).json({
-            status: "FAILED",
-            message: "No booking available",
-            booking: "",
-        });
-    }
+  if (bookings) {
+    res.status(200).json({
+      status: "00",
+      message: "Bookings retrieved successfully",
+      bookings: bookings,
+    });
+  } else {
+    res.status(401).json({
+      status: "99",
+      message: "No booking available",
+      booking: "",
+    });
+  }
 
-    // res.json(bookings);
+  // res.json(bookings);
 });
 
 export { addBookingItems, getBookingById, getMyBookings };
